9a19481d597683b526425701ef582025acb39b87
CloudHolic <bluegkstjr@kaist.ac.kr>
2022-08-18 17:04:49 +0900

    Add project files.

---
+++ TestProviderSample/ActorSystemClusterHostedService.cs
@@ -0,0 +1,24 @@
+﻿using Proto;
+using Proto.Cluster;
+
+namespace TestProviderSample;
+
+public class ActorSystemClusterHostedService : IHostedService
+{
+    private readonly ActorSystem _actorSystem;
+
+    public ActorSystemClusterHostedService(ActorSystem actorSystem)
+    {
+        _actorSystem = actorSystem;
+    }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        await _actorSystem.Cluster().StartMemberAsync();
+    }
+
+    public async Task StopAsync(CancellationToken cancellationToken)
+    {
+        await _actorSystem.Cluster().ShutdownAsync();
+    }
+}
---
+++ TestProviderSample/ActorSystemConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Proto;
+using Proto.Cluster;
+using Proto.Cluster.Testing;
+using Proto.Cluster.Partition;
+using Proto.Remote.GrpcNet;
+
+namespace TestProviderSample;
+
+public static class ActorSystemConfiguration
+{
+    public static void AddActorSystem(this IServiceCollection serviceCollection, IConfiguration configuration)
+    {
+        serviceCollection.AddSingleton(provider =>
+        {
+            var actorSystemConfig = ActorSystemConfig.Setup();
+
+            var remoteConfig = GrpcNetRemoteConfig.BindToLocalhost();
+
+            var clusterConfig = ClusterConfig
+                .Setup(
+                    "TestProviderSampleCluster",
+                    new TestProvider(new TestProviderOptions(), new InMemAgent()),
+                    new PartitionIdentityLookup())
+                .WithClusterKind(
+                    "user",
+                    Props.FromProducer(() => new UserGrainActor()));
+
+            return new ActorSystem(actorSystemConfig)
+                .WithRemote(remoteConfig)
+                .WithCluster(clusterConfig);
+        });
+    }
+}
